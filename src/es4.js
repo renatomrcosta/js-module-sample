a97a3b9a88b4456c8919e956a76c29c59aa45165
Renato Costa <Renato.RezendeCosta@otto.de>
2021-05-11 09:05:59 +0200

    initial es4 sample

---
+++ src/es4.js
@@ -0,0 +1,5 @@
+
+let es4 = require("./es4_module")
+console.log("look at that nice module import")
+console.log({es4})
+new es4()
---
+++ src/es4_module.js
@@ -0,0 +1,5 @@
+const someLambda = () => console.log("lambda called!")
+function bananas(){ console.log("I'm bananas!")}
+class SomeClass { constructor() { console.log("constructed!")}}
+
+module.exports = SomeClass
