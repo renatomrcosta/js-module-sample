465788ba0476a8a1166b9a1a417f8fab6b667d6f
Renato Costa <Renato.RezendeCosta@otto.de>
2021-05-11 09:08:02 +0200

    as javascript object

--- src/es4.js
+++ src/es4.js
@@ -1,5 +1,8 @@
 
 let es4 = require("./es4_module")
+let {bananas, SomeClass, lamb} = require("./es4_module")
 console.log("look at that nice module import")
 console.log({es4})
-new es4()
+new SomeClass()
+lamb()
+bananas()
--- src/es4_module.js
+++ src/es4_module.js
@@ -2,4 +2,8 @@ const someLambda = () => console.log("lambda called!")
 function bananas(){ console.log("I'm bananas!")}
 class SomeClass { constructor() { console.log("constructed!")}}
 
-module.exports = SomeClass
+module.exports = {
+    SomeClass,
+    bananas,
+    "lamb": someLambda
+}
