2d6dba80ebb3b7f4f6ec0b5b21336a7a5fae8607
Renato Costa <Renato.RezendeCosta@otto.de>
2021-05-11 09:12:13 +0200

    es2015 stuff

---
+++ src/es2015.js
@@ -0,0 +1,10 @@
+
+import * as MyModule from "./es2015_module"
+import { SomeClass, someLambda, bananas } from "./es2015_module"
+
+console.log("look at that nice module import")
+console.log({MyModule})
+MyModule.bananas()
+bananas()
+MyModule.someLambda()
+someLambda()
---
+++ src/es2015_module.js
@@ -0,0 +1,3 @@
+export const someLambda = () => console.log("lambda called!")
+export function bananas(){ console.log("I'm bananas!")}
+export class SomeClass { constructor() { console.log("constructed!")}}
